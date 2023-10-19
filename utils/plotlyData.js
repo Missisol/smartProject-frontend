3b130a756110b15fbfeb963e7f44817030b9c4d0
MissisOl <missisololol@gmail.com>
2023-10-20 00:37:52 +0300

    Utils added

---
+++ utils/plotlyData.js
@@ -0,0 +1,53 @@
+export const gaugeDataArr = [
+  { 
+    name: 'temperature', 
+    text: 'Температура, &deg;C',
+    reference: 30,
+    range: [null, 50],
+    color: "#3ba639",
+    steps: [
+      { range: [0, 20], color: "#fff" },
+      { range: [20, 30], color: "#aaffa9" },
+      { range: [30, 50], color: "#ef7985" },
+    ],
+    value: 40,
+  },
+  { 
+    name: 'humidity', 
+    text: 'Влажность, %', 
+    reference: 40,
+    range: [null, 100],
+    color: "#047df3",
+    steps: [
+      { range: [0, 40], color: "#fff" },
+      { range: [40, 60], color: "#a6cdf3" },
+      { range: [60, 100], color: "#70b0ef" },
+    ],
+    value: 70,
+  },
+  { 
+    name: 'pressure', 
+    text: 'Давление, мм.рт.ст.', 
+    reference: 748,
+    range: [700, 800],
+    color: "#808080",
+    steps: [
+      { range: [700, 748], color: "#fff" },
+      { range: [748, 800], color: "#cecece" },
+    ],
+    value: 760,
+  },
+]
+
+
+export const plotlyLayout = { 
+  width: 300,
+  height: 250, 
+  margin: { t: 30, b: 30, l: 30, r: 30 },
+}
+
+export const bmeDataNames = [
+  'temperature',
+  'humidity',
+  'pressure',
+]
---
+++ utils/urls.ts
@@ -0,0 +1,1 @@
+export const bmeData = '/meteo/bme/'
