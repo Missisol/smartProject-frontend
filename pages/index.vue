1972d3b96fdb17cdb4811f635157492219be6660
MissisOl <missisololol@gmail.com>
2023-10-20 00:32:25 +0300

    A page for bme data has been created

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+import { BmeChartsComponent, BmeDataComponent } from '../.nuxt/components';
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>BME Data</div>
+    <BmeDataComponent />
+    <!-- <BmeChartsComponent /> -->
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
