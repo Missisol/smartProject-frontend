c8a2f3f69ae9b6f9771685e59687c28217e00bd4
MissisOl <missisololol@gmail.com>
2023-10-20 00:31:21 +0300

    Default layout created

---
+++ components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <footer class="footer">
+    <h2>This is Footer</h2>
+  </footer>
+</template>
+
+<style lang='scss' scoped>
+.footer {
+  min-height: 100px;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <h2>This is Header</h2>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Posts</a>
+      <a href="#">Contact</a>
+    </nav>
+  </div>
+</template>
+
+
+<style lang='scss' scoped></style>
